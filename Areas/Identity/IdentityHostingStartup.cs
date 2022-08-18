da8c8a6e88cbf173bfba45251977dec106a3c597
Alexis Consuegra <aconsuegra@onehomecaresolutions.net>
2022-08-18 18:38:13 -0400

    Add project files.

---
+++ Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using xPray.Areas.Identity.Data;
+using xPray.Data;
+
+[assembly: HostingStartup(typeof(xPray.Areas.Identity.IdentityHostingStartup))]
+namespace xPray.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            //builder.ConfigureServices((context, services) => {
+            //    services.AddDbContext<AppDbContext>(options =>
+            //        options.UseSqlServer(
+            //            context.Configuration.GetConnectionString("AppDbContextConnection")));
+
+            //    services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            //        .AddEntityFrameworkStores<AppDbContext>();
+            //});
+        }
+    }
+}
